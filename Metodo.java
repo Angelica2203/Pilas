8212d51e681615a917e5f51a0d0afcb04e371b57
Sala K 402 <salak402@salasfr.itm.edu.co>
2025-03-06 07:26:07 -0500

    J

---
+++ Metodo.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Metodo {
+
+    Scanner sc = new Scanner(System.in);
+
+    public Stack<Integer> LlenarPila() {
+        Stack<Integer> pila = new Stack<>();
+        boolean bandera = true;
+        int opt = 0;
+        while (bandera) {
+            System.out.println("Ingrese el registro: ");
+            pila.push(sc.nextInt());
+            System.out.println("Desea ingresar mas registro, 1: SI y 2: NO");
+            while (!sc.hasNextInt()) {
+                System.out.println("El dato ingresado no es numerico, revise");
+                sc.next();
+            }
+            opt = sc.nextInt();
+            while (opt < 1 || opt > 2) {
+                System.out.println("Debe ingresa una opcion 1 o 2");
+            }
+            if (opt == 2) {
+                bandera = false;
+            }
+        }
+        return pila;
+
+    }
+
+    public void MostrarPila(Stack<Integer> pila) {
+        System.out.println(pila);//SOLO DATOS PRIMITIVOS SE PUEDE ASI CON OBJETOS ES DISTINTO
+    }
+
+    public Stack<Integer> EliminarDatos(Stack<Integer> pila) {
+        Stack<Integer> auxPila = new Stack<>();
+        int numero;
+        System.out.println("Ingrese el registro a eliminar");
+        numero = sc.nextInt();
+        while (!pila.isEmpty()) {
+            if (pila.peek() ==) {
+                
+            }
+        }
+    }
+}
---
+++ Pila.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Pila {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void pila() {
+        Stack<Integer> pila = new Stack<>(); //palabra para declarar la pila es stack para trabajar con enteros es integer eso va entre <>
+        Metodo m = new Metodo();
+        pila = m.LlenarPila();
+
+    }
+
+}
